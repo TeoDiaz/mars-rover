e488f5a6389b8ce64f8c9532f6be9ea20df13a3d
Teo Diaz <tdiazsan@gmail.com>
2018-08-06 19:15:16 +0200

    mas cambios

--- app.js
+++ app.js
@@ -15,7 +15,12 @@ var rover2 = {
 
 var roverHtml = document.getElementById("rover");
 var rover2Html = document.getElementById("rover2");
-
+document.getElementById("seleRover1").onclick = function() {clickBoton1(rover)};
+document.getElementById("seleRover2").onclick = function() {clickBoton2(rover2)};
+document.getElementById("forward").onclick = function() {moveForward(rover)};
+document.getElementById("backward").onclick = function() {moveBackward(rover)};
+document.getElementById("left").onclick = function() {turnLeft(rover)};
+document.getElementById("right").onclick = function() {turnRight(rover)};
 
 var onHab = false;
 var botonRover1 = false;
@@ -136,7 +141,7 @@ function moveForward(rover){
             var nextPosition = superficie[rover.y][rover.x -1];
             if(nextPosition == "O"){
               console.log("Cuidado! Un obstaculo");
-              alert("Cuidado, acabas de pinchar con una rueda");
+              alert("Cuidado, acabas de pinchar con una piedra");
             }else if(nextPosition == "R2"){
               console.log("Cuidado, hay otro Rover delante, no lo ves?");
               alert("Cuidado, hay un Rover delante");
@@ -157,223 +162,7 @@ function moveForward(rover){
     rover.travelLog.push("F");
     console.log(rover);
     console.log(superficie);
-  }
-}
-
-
-
-function moveBackward(rover){
-  if(botonRover1 == false  && botonRover2 == false){
-    alert("Selecciona un Rover");
-  }else if(botonRover1){ 
-    console.log("Hacia atras!");
-    if(onHab){
-      superficie[rover.y][rover.x] = "HAB";
-      onHab = false;
-    }else{
-      superficie[rover.y][rover.x] = null;
-    }
-    switch (rover.direction){
-      case "N":
-        if(rover.y == 9){
-          console.log("Cuidado, te sales del planeta!");
-        }else{
-          var nextPosition = superficie[rover.y + 1][rover.x];
-          if(nextPosition == "O"){
-            console.log("Cuidado! Un obstaculo");
-            alert("Cuidado, te has chocado con THE ROCK");
-          }else if(nextPosition == "R2"){
-            console.log("Cuidado, hay otro Rover delante, no lo ves?");
-            alert("Cuidado, hay un Rover delante");
-          }else{
-            rover.y += 1;
-            var pos = rover.y * 50 + 25;
-            roverHtml.style.top = pos+"px";
-            if(nextPosition == "HAB"){
-              onHab = true;
-              console.log("Has llegado al HAB! Estas a salvo");
-              alert("HOGAR, DULCE HOGAR");
-            }
-          }
-        }
-      break;
-
-      case "S":
-        if(rover.y == 0){
-          console.log("Cuidado, te sales del planeta!");
-        }else{
-          var nextPosition = superficie[rover.y - 1][rover.x];
-          if(nextPosition == "O"){
-            console.log("Cuidado! Un obstaculo");
-            alert("Cuidado, que te chocas");
-          }else if(nextPosition == "R2"){
-            console.log("Cuidado, hay otro Rover delante, no lo ves?");
-            alert("Cuidado, hay un Rover delante");
-          }else{
-            rover.y -= 1;
-            var pos = rover.y * 50 + 25;
-            roverHtml.style.top = pos+"px";
-            if(nextPosition == "HAB"){
-              onHab = true;
-              console.log("Has llegado al HAB! Estas a salvo");
-              alert("MATT, ESTAS AHI?");
-            }
-          }
-        }
-      break;
-
-      case "E":
-        if(rover.x == 0){
-          console.log("Cuidado, te sales del planeta!");
-        }else{
-          var nextPosition = superficie[rover.y][rover.x - 1];
-          if(nextPosition == "O"){
-            console.log("Cuidado! Un obstaculo");
-            alert("Cuidado, una pedazo de piedra");
-          }else if(nextPosition == "R2"){
-            console.log("Cuidado, hay otro Rover delante, no lo ves?");
-            alert("Cuidado, hay un Rover delante");
-          }else{
-            rover.x -=1;
-            var pos = rover.x * 50 + 25;
-            roverHtml.style.left = pos+"px";          
-            if(nextPosition == "HAB"){
-              onHab = true;
-              console.log("Has llegado al HAB! Estas a salvo");
-              alert("MAMAAAA ESTOY EN CASA!!");
-            }
-          }
-        }
-      break;
-
-      case "W":
-        if(rover.x == 9){
-          console.log("Cuidado, te sales del planeta");
-        }else{
-          var nextPosition = superficie[rover.y][rover.x + 1];
-          if(nextPosition == "O"){
-            console.log("Cuidado! Un Obstaculo");
-            alert("Cuidado, una Roca");
-          }else if(nextPosition == "R2"){
-            console.log("Cuidado, hay otro Rover delante, no lo ves?");
-            alert("Cuidado, hay un Rover delante");
-          }else{
-            rover.x +=1;
-            var pos = rover.x * 50 + 25;
-            roverHtml.style.left = pos+"px";
-            if(nextPosition == "HAB"){
-              onHab = true;
-              console.log("Has llegado al HAB! Estas a salvo");  
-              alert("QUIERO MI BOCADILLO!!");
-            }
-          }
-        }    
-      break;
-    }
-
-
-  superficie[rover.y][rover.x] = "R";
-  rover.travelLog.push("B");
-  console.log(rover);
-  console.log(superficie);
-  }
-}
-
-
-
-function turnLeft(rover){
-  if(botonRover1 == false && botonRover2 == false){
-    alert("Selecciona un Rover!");
-  }else if(botonRover1){
-    console.log("Gira a la izquierda!");
-    switch (rover.direction){
-      case "N":
-        rover.direction = "W";
-        roverHtml.style.webkitTransform = "rotate("+270+"deg)";
-      break;
-
-      case "W":
-        rover.direction = "S";
-        roverHtml.style.webkitTransform = "rotate("+180+"deg)";
-      break;
-
-      case "S":
-        rover.direction = "E";
-        roverHtml.style.webkitTransform = "rotate("+90+"deg)";
-      break;
-
-      case "E":
-        rover.direction = "N";
-        roverHtml.style.webkitTransform = "rotate("+0+"deg)";
-      break;
-    }
-    console.log(superficie);
-    console.log (rover);
-  }
-}
-
-
-function turnRight(rover){
-  if(botonRover1 == false && botonRover2 == false){
-    alert("Selecciona un Rover!");
-  }else if(botonRover1){
-    console.log("Gira a la derecha!");
-    switch (rover.direction){
-      case "N":
-        rover.direction = "E";
-        roverHtml.style.webkitTransform = "rotate("+90+"deg)";
-      break;
-
-      case "E":
-        rover.direction = "S";
-        roverHtml.style.webkitTransform = "rotate("+180+"deg)";
-      break;
-
-      case "S":
-        rover.direction = "W";
-        roverHtml.style.webkitTransform = "rotate("+270+"deg)";
-      break;
-
-      case "W":
-        rover.direction = "N";
-        roverHtml.style.webkitTransform = "rotate("+0+"deg)";
-      break;
-    }
-    console.log(rover);
-  }
-}
-
-
-function commands(command){
-  for(var i = 0; i<command.length; i++){
-    console.log(command[i]);
-    switch (command[i]) {
-      case "f":
-        moveForward(rover);
-      break;
-      
-      case "b":
-        moveBackward(rover);
-      break;
-
-      case "l":
-        turnLeft(rover);
-      break;
-
-      case "r":
-        turnRight(rover);
-      break;
-
-      default:
-        console.log("Eso no va a funcionar!");
-      break;
-    
-    }    
-  }
-}
-
-function moveForward2(rover2){
-  if(botonRover2){
+  }else if(botonRover2){
     console.log("Avanzas!");
     if(onHab){
       superficie[rover2.y][rover2.x] = "HAB";
@@ -486,9 +275,119 @@ function moveForward2(rover2){
   }
 }
 
+function moveBackward(rover){
+  if(botonRover1 == false  && botonRover2 == false){
+    alert("Selecciona un Rover");
+  }else if(botonRover1){ 
+    console.log("Hacia atras!");
+    if(onHab){
+      superficie[rover.y][rover.x] = "HAB";
+      onHab = false;
+    }else{
+      superficie[rover.y][rover.x] = null;
+    }
+    switch (rover.direction){
+      case "N":
+        if(rover.y == 9){
+          console.log("Cuidado, te sales del planeta!");
+        }else{
+          var nextPosition = superficie[rover.y + 1][rover.x];
+          if(nextPosition == "O"){
+            console.log("Cuidado! Un obstaculo");
+            alert("Cuidado, te has chocado con THE ROCK");
+          }else if(nextPosition == "R2"){
+            console.log("Cuidado, hay otro Rover delante, no lo ves?");
+            alert("Cuidado, hay un Rover delante");
+          }else{
+            rover.y += 1;
+            var pos = rover.y * 50 + 25;
+            roverHtml.style.top = pos+"px";
+            if(nextPosition == "HAB"){
+              onHab = true;
+              console.log("Has llegado al HAB! Estas a salvo");
+              alert("HOGAR, DULCE HOGAR");
+            }
+          }
+        }
+      break;
 
-function moveBackward2(rover2){
-  if(botonRover2){
+      case "S":
+        if(rover.y == 0){
+          console.log("Cuidado, te sales del planeta!");
+        }else{
+          var nextPosition = superficie[rover.y - 1][rover.x];
+          if(nextPosition == "O"){
+            console.log("Cuidado! Un obstaculo");
+            alert("Cuidado, que te chocas");
+          }else if(nextPosition == "R2"){
+            console.log("Cuidado, hay otro Rover delante, no lo ves?");
+            alert("Cuidado, hay un Rover delante");
+          }else{
+            rover.y -= 1;
+            var pos = rover.y * 50 + 25;
+            roverHtml.style.top = pos+"px";
+            if(nextPosition == "HAB"){
+              onHab = true;
+              console.log("Has llegado al HAB! Estas a salvo");
+              alert("MATT, ESTAS AHI?");
+            }
+          }
+        }
+      break;
+
+      case "E":
+        if(rover.x == 0){
+          console.log("Cuidado, te sales del planeta!");
+        }else{
+          var nextPosition = superficie[rover.y][rover.x - 1];
+          if(nextPosition == "O"){
+            console.log("Cuidado! Un obstaculo");
+            alert("Cuidado, una pedazo de piedra");
+          }else if(nextPosition == "R2"){
+            console.log("Cuidado, hay otro Rover delante, no lo ves?");
+            alert("Cuidado, hay un Rover delante");
+          }else{
+            rover.x -=1;
+            var pos = rover.x * 50 + 25;
+            roverHtml.style.left = pos+"px";          
+            if(nextPosition == "HAB"){
+              onHab = true;
+              console.log("Has llegado al HAB! Estas a salvo");
+              alert("MAMAAAA ESTOY EN CASA!!");
+            }
+          }
+        }
+      break;
+
+      case "W":
+        if(rover.x == 9){
+          console.log("Cuidado, te sales del planeta");
+        }else{
+          var nextPosition = superficie[rover.y][rover.x + 1];
+          if(nextPosition == "O"){
+            console.log("Cuidado! Un Obstaculo");
+            alert("Cuidado, una Roca");
+          }else if(nextPosition == "R2"){
+            console.log("Cuidado, hay otro Rover delante, no lo ves?");
+            alert("Cuidado, hay un Rover delante");
+          }else{
+            rover.x +=1;
+            var pos = rover.x * 50 + 25;
+            roverHtml.style.left = pos+"px";
+            if(nextPosition == "HAB"){
+              onHab = true;
+              console.log("Has llegado al HAB! Estas a salvo");  
+              alert("QUIERO MI BOCADILLO!!");
+            }
+          }
+        }    
+      break;
+    }
+    superficie[rover.y][rover.x] = "R";
+    rover.travelLog.push("B");
+    console.log(rover);
+    console.log(superficie);
+  }else if(botonRover2){
     console.log("Hacia atras!");
     if(onHab){
       superficie[rover2.y][rover2.x] = "HAB";
@@ -593,7 +492,6 @@ function moveBackward2(rover2){
         }
       break;
     }
-
     superficie[rover2.y][rover2.x] = "R2";
     rover2.travelLog.push("B");
     console.log(rover2);
@@ -601,10 +499,35 @@ function moveBackward2(rover2){
   }
 }
 
+function turnLeft(rover){
+  if(botonRover1 == false && botonRover2 == false){
+    alert("Selecciona un Rover!");
+  }else if(botonRover1){
+    console.log("Gira a la izquierda!");
+    switch (rover.direction){
+      case "N":
+        rover.direction = "W";
+        roverHtml.style.webkitTransform = "rotate("+270+"deg)";
+      break;
 
+      case "W":
+        rover.direction = "S";
+        roverHtml.style.webkitTransform = "rotate("+180+"deg)";
+      break;
 
-function turnLeft2(rover2){
-  if(botonRover2){
+      case "S":
+        rover.direction = "E";
+        roverHtml.style.webkitTransform = "rotate("+90+"deg)";
+      break;
+
+      case "E":
+        rover.direction = "N";
+        roverHtml.style.webkitTransform = "rotate("+0+"deg)";
+      break;      
+    }    
+    console.log (rover);
+
+  }else if(botonRover2){
     console.log("Gira a la izquierda!");
     switch (rover2.direction){
       case "N":
@@ -631,8 +554,34 @@ function turnLeft2(rover2){
   }
 }
 
-function turnRight2(rover2){
-  if(botonRover2){
+function turnRight(rover){
+  if(botonRover1 == false && botonRover2 == false){
+    alert("Selecciona un Rover!");
+  }else if(botonRover1){
+    console.log("Gira a la derecha!");
+    switch (rover.direction){
+      case "N":
+        rover.direction = "E";
+        roverHtml.style.webkitTransform = "rotate("+90+"deg)";
+      break;
+
+      case "E":
+        rover.direction = "S";
+        roverHtml.style.webkitTransform = "rotate("+180+"deg)";
+      break;
+
+      case "S":
+        rover.direction = "W";
+        roverHtml.style.webkitTransform = "rotate("+270+"deg)";
+      break;
+
+      case "W":
+        rover.direction = "N";
+        roverHtml.style.webkitTransform = "rotate("+0+"deg)";
+      break;
+    }
+    console.log(rover);
+  }else if(botonRover2){
     console.log("Gira a la derecha!");
     switch (rover2.direction){
       case "N":
@@ -659,24 +608,24 @@ function turnRight2(rover2){
   }
 }
 
-function commands2(command2){
-  for(var i = 0; i<command2.length; i++){
-    console.log(command2[i]);
-    switch (command2[i]) {
+function commands(command){
+  for(var i = 0; i<command.length; i++){
+    console.log(command[i]);
+    switch (command[i]) {
       case "f":
-        moveForward2(rover2);
+        moveForward(rover);
       break;
       
       case "b":
-        moveBackward2(rover2);
+        moveBackward(rover);
       break;
 
       case "l":
-        turnLeft2(rover2);
+        turnLeft(rover);
       break;
 
       case "r":
-        turnRight2(rover2);
+        turnRight(rover);
       break;
 
       default:
